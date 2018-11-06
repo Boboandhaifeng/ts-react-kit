fe050f3ef3de605076c41abbd3d8b6a4e95cb4cd
bao <913239808@qq.com>
2018-11-06 10:56:42 +0800

    img download

--- src/pages/test/home/index.tsx
+++ src/pages/test/home/index.tsx
@@ -1,6 +1,8 @@
 import * as React from "react"
-import { Ant } from "../../../components/Ant"
-import axios from '../../../utils/axios'
+import { Ant } from "components/Ant"
+import axios from 'utils/axios'
+// const testImg = require('images/erwei.jpg')
+const testImg = 'https://static.mobike.com/wx/distribution-banner-normal-3ae3892650.png'
 
 export interface AntProps {
   name: string
@@ -15,7 +17,10 @@ export class _Home extends React.Component<AntProps, {}> {
   }
   render() {
     return (
+      <div>
         <Ant name="home" company="mobike" />
+        <a href={testImg} download="img" >下载图片</a>
+      </div>
     )
   }
 }
