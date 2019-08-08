cb64d111d30b716815b68a97fc29db2daae379af
包智文 <baozhiwen@58.com>
2019-08-08 14:37:01 +0800

    test 组件

--- src/pages/test/home/index.tsx
+++ src/pages/test/home/index.tsx
@@ -1,10 +1,12 @@
 import * as React from "react"
 import { Ant } from "components/Ant"
+import { FootBar } from "components/FootBar/index.tsx"
 import { Button } from "antd"
 import axios from 'utils/axios'
 import { downloadImage } from 'utils/utils'
 // const testImg = require('images/erwei.jpg')
 const testImg = 'https://static.mobike.com/wx/distribution-banner-normal-3ae3892650.png'
+const defaultImg = 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1565256163938&di=1142a1e0d6d004fab6e6bc0d7a63edbc&imgtype=0&src=http%3A%2F%2Fwww.wndhw.com%2Ffengjing%2Ftouxiang%2Fimages%2Ftx001t1.jpg'
 
 export interface AntProps {
   name: string
@@ -19,9 +21,14 @@ export class _Home extends React.Component<AntProps, {}> {
     this.watchStorage()
   }
   render() {
+    const info = {
+      avatar: defaultImg,
+      userName: '铁蛋',
+    }
     return (
       <div>
         <Ant name="home" company="mobike" />
+        <FootBar info={info} wxClickCb={() => {}} phoneClickCb={() => {}} />
         {/* <div>
           <a href={testImg} download="img" >下载图片</a>
         </div> */}
